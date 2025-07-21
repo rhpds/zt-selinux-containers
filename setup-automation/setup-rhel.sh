edf91a87442e4361ea447ee2a4986b3a51926f17
Jscar Hawk <57020043+jscar-hawk@users.noreply.github.com>
2025-07-21 14:34:06 -0500

    Add RHEL 10 Repos

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -4,3 +4,9 @@ usermod -aG wheel rhel
 
 buildah rm -a
 setsebool -P container_manage_cgroup true
+
+# install the packages
+dnf install -y --releasever=10 --installroot=$scratchmnt redhat-release
+dnf install -y --setopt=reposdir=/etc/yum.repos.d \
+      --installroot=$scratchmnt \
+      --setopt=cachedir=/var/cache/dnf httpd
