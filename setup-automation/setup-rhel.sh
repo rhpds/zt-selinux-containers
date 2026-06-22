a65a904c852e1e398d0800ce8d055fecae0c9ddd
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-06-22 10:23:20 -0400

    Unregister and re-register the system

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Unregister and register the VM
+subscription-manager clean
+subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
+
 useradd rhel
 usermod -aG wheel rhel
 
